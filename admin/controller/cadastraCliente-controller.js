468b2a50e02b10bd8772dc061a995df87720a93b
João Vitor Diniz <jvsdiniz@gmail.com>
2021-07-04 19:02:49 -0300

    Adicionando função de Cadastrar Clientes

---
+++ admin/controller/cadastraCliente-controller.js
@@ -0,0 +1,14 @@
+import { clienteService } from "../service/cliente-service.js"
+
+const formulario = document.querySelector('[data-form]')
+
+addEventListener('submit' , (event) => {
+    event.preventDefault()
+    const nome = event.target.querySelector('[data-nome]').value
+    const email = event.target.querySelector('[data-email]').value
+
+    clienteService.cadastraCliente(nome, email)
+    .then(() => {
+        window.location.href = '../telas/cadastro_concluido.html'
+    })
+})
--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -1,5 +1,5 @@
 //*****Abaixo é aberta a comunicação ajax (aplicação x API) assíncrona para alimentar a tbody*****
-//***** A fetch já faz o GET e substituiu a promise e o http (bloco abaixo)
+//***** A fetch substituiu a promise e o http (bloco abaixo)
     /* const promise = new Promise((resolve, reject)=>{
         const http = new XMLHttpRequest()  
         http.open('GET', 'http://localhost:3000/profile') // (1º modo de request , 2º endereço a enviar a requisição)
@@ -17,13 +17,30 @@
 //caso ocorra erro de CORS execute o comando: browser-sync start --server --file . --host --port 5000 -startPath admin/telas/lista_cliente.html
 
 const listaClientes = () => {
-    return fetch(`http://localhost:3000/profile`)
+    return fetch(`http://localhost:3000/profile`)//fetch tem o "get" automático
     .then(resposta=>{
         console.log(resposta)
         return resposta.json()
     }) 
 }
 
-export const clienteService ={
-    listaClientes
+const cadastraCliente = (nome, email) =>{
+    return fetch(`http://localhost:3000/profile`,{
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json' //tipo de conteúdo enviado
+        },
+        body: JSON.stringify({ //dados enviados pelo form
+            nome: nome, 
+            email: email
+        })
+    }) 
+    .then (resposta =>{
+        return resposta.body
+    })
+}
+
+export const clienteService = {
+    listaClientes,
+    cadastraCliente
 }
